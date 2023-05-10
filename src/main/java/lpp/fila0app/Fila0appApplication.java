09e77c1084c1da4c77e97ff1e34c2c8581465cbb
alejo <alejocrrs@gmail.com>
2023-05-09 23:41:39 -0500

    Creación proyecto

---
+++ src/main/java/lpp/fila0app/Fila0appApplication.java
@@ -0,0 +1,13 @@
+package lpp.fila0app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fila0appApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fila0appApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lpp/fila0app/Fila0appApplicationTests.java
@@ -0,0 +1,13 @@
+package lpp.fila0app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fila0appApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
